d035020b72cbff3733f425c75bc15fc4747b3c9d
MdPiarulIslamAlif <mdpiarulislamalif7@gmail.com>
2024-01-05 09:47:01 +0600

    added a footer and home page or share folder

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+     return (
+          <div>
+           this is my Home component     
+          </div>
+     );
+};
+
+export default Home;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main/Main";
+import Home from "../Pages/Home/Home";
 
 
 export const router = createBrowserRouter([
      {
           path: "/",
-          element: <div>hello world !</div>
+          element: <Main />,
+          children:[
+               {
+                   path:"/",
+                  element:<Home /> 
+               }
+          ]
      }
 ])
